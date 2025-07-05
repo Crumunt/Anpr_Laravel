115294ca66296207eefe6841da17e7bef7fbb984
Crumunt <lorenz.bocatot@clsu2.edu.ph>
2025-07-05 22:49:59 +0800

    FIXED ROUTING ERROR ON ADMIN NAV

--- resources/views/components/nav/navbar.blade.php
+++ resources/views/components/nav/navbar.blade.php
@@ -33,7 +33,7 @@
     <x-nav.partial.sidebar-section title="Users">
         <x-nav.partial.sidebar-item 
             title="Applicant Form" 
-            routeName="admin.gate-pass-applicant-form"
+            routeName="gate-pass.gate-pass-applicant-form"
             icon="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
         />
     </x-nav.partial.sidebar-section>
