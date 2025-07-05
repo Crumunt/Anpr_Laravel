3b5ebe3ec11a710efc316b875e456bb534bb4657
Crumunt <lorenz.bocatot@clsu2.edu.ph>
2025-07-05 19:24:12 +0800

    FIXED ROUTES

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,18 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/gate-pass', fn() => view('gate-pass'))->name('gate-pass');
     Route::get('/vehicles', fn() => view('vehicles'))->name('vehicles');
     Route::get('/admins', fn() => view('admins'))->name('admins');
-    Route::get('/gate-pass-applicant-form', fn() => view('gate-pass-applicant-form'))->name('gate-pass-applicant-form');
-
+    
 });
 
 //sa applicant form route
 
 Route::prefix('gate-pass')->name('gate-pass.')->group(function () {
-    Route::get('/personal-info', fn() => view('personal-info'))->name('personal-info');
-    Route::get('/documents', fn() => view('documents'))->name('documents');
-    Route::get('/vehicle', fn() => view('vehicle-info'))->name('vehicle');
-    Route::get('/review', fn() => view('review'))->name('review');
-    Route::get('/success', fn() => view('success'))->name('success');
+    Route::get('/gate-pass-applicant-form', fn() => view('gate-pass-applicant-form'))->name('gate-pass-applicant-form');
+    Route::get('/personal-info', fn() => view('gate-pass.personal-info'))->name('personal-info');
+    Route::get('/documents', fn() => view('gate-pass.documents'))->name('documents');
+    Route::get('/vehicle', fn() => view('gate-pass.vehicle-info'))->name('vehicle');
+    Route::get('/review', fn() => view('gate-pass.review'))->name('review');
+    Route::get('/success', fn() => view('gate-pass.success'))->name('success');
 });
 
 
