e00e6ec072ff7089e8d8072f50c0246e30d49190
Crumunt <lorenz.bocatot@clsu2.edu.ph>
2025-07-12 23:14:59 +0800

    FIXED BROKEN PAGE

--- resources/views/components/details/layout.blade.php
+++ resources/views/components/details/layout.blade.php
@@ -8,23 +8,26 @@
 
 @section('main-content')
 
-<div class="container mx-auto py-6 px-4 max-w-8xl">
-    {{ $breadcrumb ?? '' }}
-    {{ $header ?? '' }}
-    
-    <!-- Main Content -->
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <!-- Left Column - Main Information -->
-      <div class="lg:col-span-2 space-y-6">
-        {{ $mainContent ?? '' }}
-      </div>
+<div class="flex-1 md:ml-64 p-6 pt-20">
+
+  <div class="container mx-auto py-6 px-4 max-w-8xl">
+      {{ $breadcrumb ?? '' }}
+      {{ $header ?? '' }}
       
-      <!-- Right Column - Status, Documents, Activity -->
-      <div class="space-y-6">
-        {{ $sideContent ?? '' }}
+      <!-- Main Content -->
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <!-- Left Column - Main Information -->
+        <div class="lg:col-span-2 space-y-6">
+          {{ $mainContent ?? '' }}
+        </div>
+        
+        <!-- Right Column - Status, Documents, Activity -->
+        <div class="space-y-6">
+          {{ $sideContent ?? '' }}
+        </div>
       </div>
-    </div>
   </div>
+</div>
   
   {{ $modals ?? '' }}
 @endsection
